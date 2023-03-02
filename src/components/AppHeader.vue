b3bb4f09915f31d4214521780a3fd95f433c24cc
Vale <oirelav95@gmail.com>
2023-03-02 13:44:09 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      Sono un header
+    </h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
