5832de78a3c187d4baded7ce2a9241140c253324
Vale <oirelav95@gmail.com>
2023-09-12 22:23:40 +0200

    create component Presentation.vue

---
+++ src/components/Presentation.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'Presentation',
+}
+</script>
+
+<template>
+    <section>
+        <h1>
+            Ciao,
+            <div>sono Valerio</div>
+        </h1>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+
+section {
+    h1 {
+        font-size: 4rem;
+        font-weight: 400;
+    }
+}
+</style>
