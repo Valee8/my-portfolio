f27511aafc4e151aea5d9431c792a3be20ba2b5f
Vale <oirelav95@gmail.com>
2023-09-14 01:04:20 +0200

    create component Skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,21 +3,18 @@ export default {
     name: 'Projects',
     data() {
         return {
-            listProjects: [
+            listSkills: [
                 {
-                    title: "social-network",
-                    description: "A project that aims to improve skills and learn new technologies. Initially, the focus is on learning and designing the business layer, not on the UI.",
-                    technologies: "Typescript, React, Node.js"
+                    name: "nodejs",
+                    icon: "fa-brands fa-node",
                 },
                 {
-                    title: "multilingual-json-split",
-                    description: "Split the JSON into multiple files of each language",
-                    technologies: "JavaScripts"
+                    name: "git",
+                    icon: "",
                 },
                 {
-                    title: "jwt-auth",
-                    description: "The focus is on learning and designing the business layer, not on the UI",
-                    technologies: "Typescript, React, Node.js"
+                    name: "javascript",
+                    icon: "fa-brands fa-js",
                 }
             ]
         }
@@ -30,6 +27,13 @@ export default {
         <h2>
             <span>&lt;</span>Skills<span>&gt;</span>
         </h2>
+
+        <div class="list-skills">
+            <div class="skill" :class="skill.name" v-for="(skill, index) in listSkills" :key="index">
+                <span class="icon" :class="{ 'none': skill.icon === '' }"><i :class="skill.icon"></i></span>
+                <span class="name">&num;{{ skill.name }}</span>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -37,4 +41,50 @@ export default {
 @use '../src/styles/general.scss' as *;
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
+
+section {
+    .list-skills {
+        display: flex;
+        padding-top: 50px;
+
+        .skill {
+            display: flex;
+            align-items: center;
+            gap: 0 10px;
+            font-weight: 400;
+            border-radius: 4px;
+            padding: 10px 20px;
+            font-size: 1.4rem;
+            margin: 5px;
+
+            .icon {
+                font-size: 1.6rem;
+
+                &.none {
+                    display: none;
+                }
+            }
+
+            &.nodejs {
+                background-color: #66b35d;
+                color: #f0f1f9;
+
+                .icon {
+                    color: #3c5250;
+                }
+            }
+
+            &.git {
+                background-color: #f0f1f9;
+                color: #0b243d;
+            }
+
+            &.javascript {
+                background-color: #33a668;
+                color: #f7df1e;
+            }
+        }
+
+    }
+}
 </style>
