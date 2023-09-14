ef75377f907b6fd6e922d4812b27eb0976f1fc20
Vale <oirelav95@gmail.com>
2023-09-14 10:51:16 +0200

    add link contact in nav

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -18,7 +18,7 @@ export default {
                 },
                 {
                     text: "Contact",
-                    link: "#"
+                    link: "#contact"
                 }
             ],
             navSocials: [
