c205b49c19f5789d4116e07beb139ae487b72163
Vale <oirelav95@gmail.com>
2023-09-14 10:51:24 +0200

    create component Contact.vue

---
+++ src/components/Contact.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'Contact',
+}
+</script>
+
+<template>
+    <section id="contact">
+        <h2>
+            <span>&lt;</span>Contact me<span>&gt;</span>
+        </h2>
+
+        <p>
+            Ciao
+        </p>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
